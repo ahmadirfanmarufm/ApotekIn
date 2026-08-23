03cf3074a7a74de870ec313283fd6061f569b553
Terzy <terzy05@gmail.com>
2026-08-24 00:17:17 +0700

    feat : AIinsight dan fungsinya

--- middleware.ts
+++ middleware.ts
@@ -7,9 +7,7 @@ export default withAuth({
 });
 
 export const config = {
-  // Matikan sementara untuk keperluan slicing UI
-  // matcher: [
-  //   "/((?!api/auth|api|_next/static|_next/image|favicon.ico|images|login).*)",
-  // ],
-  matcher: [],
+  matcher: [
+    "/((?!api/auth|api|_next/static|_next/image|favicon.ico|images|login).*)",
+  ],
 };
